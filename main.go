3bbaaae9990d17e5a06a9f3a1a762ea79658238e
Anton Osenenko <418868+a0s@users.noreply.github.com>
2020-08-24 04:23:47 +0300

    add subscription to candles

--- main.go
+++ main.go
@@ -11,9 +11,15 @@ import (
 )
 
 var (
-	token         = flag.String("token", "", "your sandbox's token")
-	tickers       = flag.String("tickers", "", "list of tickers")
-	depth         = flag.Int("depth", 1, "depth of orderbook")
+	token = flag.String("token", "", "your sandbox's token")
+	path  = flag.String("path", ".", "path to storage dir")
+
+	orderbook      = flag.String("orderbook", "", "list of tickers to subscribe for orderbooks")
+	orderbookDepth = flag.Int("orderbook-depth", 1, "depth of orderbook")
+
+	candle         = flag.String("candle", "", "list of tickers to subscribe for candles")
+	candleInterval = flag.String("candleInterval", "1min", "interval of candles: 1min,2min,3min,5min,10min,15min,30min,hour,2hour,4hour,day,week,month")
+
 	version       = flag.Bool("version", false, "show version info")
 	VersionString string
 )
@@ -35,26 +41,17 @@ func main() {
 	}
 	defer streamingClient.Close()
 
-	scope := NewMainScope(flagTickers(*tickers))
-	scope.initInstruments(sandboxRestClient, logger)
+	scope := NewMainScope(listToTickers(*orderbook), listToTickers(*candle), logger)
+	scope.initInstruments(sandboxRestClient)
 	scope.initChannels()
-	scope.initDiskWriters(logger)
+	scope.initDiskWriters()
 
-	go scope.eventReceiver(streamingClient, logger)
+	go scope.eventReceiver(streamingClient)
 
-	scope.subscribeOrderbooks(streamingClient, logger)
-	defer scope.unsubscribeOrderbooks(streamingClient, logger)
+	scope.subscribeOrderbooks(streamingClient)
+	scope.subscribeCandles(streamingClient)
+	defer scope.unsubscribeOrderbooks(streamingClient)
+	defer scope.unsubscribeCandles(streamingClient)
 
 	select {} // sleep(0), epta
-}
-
-var letterRunes = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
-
-func requestID() string {
-	b := make([]rune, 12)
-	for i := range b {
-		b[i] = letterRunes[rand.Intn(len(letterRunes))]
-	}
-
-	return string(b)
 }
--- main_scope.go
+++ main_scope.go
@@ -6,49 +6,59 @@ import (
 	"fmt"
 	sdk "github.com/TinkoffCreditSystems/invest-openapi-go-sdk"
 	"log"
+	"math/rand"
 	"os"
 	"path/filepath"
 	"strings"
 	"time"
 )
 
-type ticker string
-type figi string
+type ticker string // eg. MSFT
+type figi string   // eg. BBG000BPH459
 
 type wrappedEvent struct {
 	time   time.Time
 	ticker ticker
-	event  sdk.OrderBookEvent
+	event  interface{}
 }
 type eventChannel chan *wrappedEvent
 
 type mainScope struct {
-	tickers          []ticker
+	orderbookTickers []ticker
+	candleTickers    []ticker
+
+	orderbookFigiChannels map[figi]eventChannel
+	candlesFigiChannels   map[figi]eventChannel
+
 	figiInstrument   map[figi]sdk.Instrument
 	tickerInstrument map[ticker]sdk.Instrument
-	channels         map[figi]eventChannel
+
+	logger *log.Logger
 }
 
-func NewMainScope(tickers []ticker) *mainScope {
+func NewMainScope(orderbookTickers []ticker, candleTickers []ticker, logger *log.Logger) *mainScope {
 	return &mainScope{
-		tickers:          tickers,
+		orderbookTickers: orderbookTickers,
+		candleTickers:    candleTickers,
+
+		orderbookFigiChannels: map[figi]eventChannel{},
+		candlesFigiChannels:   map[figi]eventChannel{},
+
 		figiInstrument:   map[figi]sdk.Instrument{},
 		tickerInstrument: map[ticker]sdk.Instrument{},
-		channels:         map[figi]eventChannel{},
+
+		logger: logger,
 	}
 }
 
-func (s *mainScope) initInstruments(restClient *sdk.SandboxRestClient, logger *log.Logger) {
+func (s *mainScope) initInstruments(restClient *sdk.SandboxRestClient) {
 TICKERS:
-	for _, ticker := range s.tickers {
+	for _, ticker := range s.allTickers() {
 		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-
-		// Hmm, Possible resource leak, 'defer' is called in a 'for' loop
-		defer cancel()
 
 		foundInstruments, err := restClient.InstrumentByTicker(ctx, string(ticker))
 		if err != nil {
-			logger.Fatalln(err)
+			s.logger.Fatalln(err)
 		}
 		for _, instrument := range foundInstruments {
 			if instrument.Ticker == string(ticker) {
@@ -57,103 +67,249 @@ TICKERS:
 				continue TICKERS
 			}
 		}
+
+		cancel()
 	}
 }
 
 func (s *mainScope) initChannels() {
-	for _, instrument := range s.tickerInstrument {
-		s.channels[figi(instrument.FIGI)] = make(eventChannel)
+	for _, ticker := range s.allTickers() {
+		instrument := s.tickerInstrument[ticker]
+		figi := figi(instrument.FIGI)
+
+		if _, ok := findTicker(s.orderbookTickers, ticker); ok {
+			s.orderbookFigiChannels[figi] = make(eventChannel)
+		}
+
+		if _, ok := findTicker(s.candleTickers, ticker); ok {
+			s.candlesFigiChannels[figi] = make(eventChannel)
+		}
 	}
 }
 
-func (s *mainScope) eventReceiver(streamingClient *sdk.StreamingClient, logger *log.Logger) {
+func (s *mainScope) eventReceiver(streamingClient *sdk.StreamingClient) {
 	err := streamingClient.RunReadLoop(func(event interface{}) error {
-		e := event.(sdk.OrderBookEvent)
-		figi := figi(e.OrderBook.FIGI)
-		ticker := ticker(s.figiInstrument[figi].Ticker)
+		var t ticker
+		var f figi
+
+		switch realEvent := event.(type) {
+		case sdk.OrderBookEvent:
+			f = figi(realEvent.OrderBook.FIGI)
+		case sdk.CandleEvent:
+			f = figi(realEvent.Candle.FIGI)
+		default:
+			s.logger.Fatalln("unsupport event type", event)
+		}
+		t = ticker(s.figiInstrument[f].Ticker)
 
 		ce := wrappedEvent{
 			time:   time.Now(),
-			ticker: ticker,
-			event:  e,
+			ticker: t,
+			event:  event,
 		}
 
-		s.channels[figi] <- &ce
+		switch event.(type) {
+		case sdk.OrderBookEvent:
+			s.orderbookFigiChannels[f] <- &ce
+		case sdk.CandleEvent:
+			s.candlesFigiChannels[f] <- &ce
+		}
+
 		return nil
 	})
 	if err != nil {
-		logger.Fatalln(err)
+		s.logger.Fatalln(err)
 	}
 }
 
-func (s *mainScope) subscribeOrderbooks(streamingClient *sdk.StreamingClient, logger *log.Logger) {
-	for _, instrument := range s.tickerInstrument {
-		err := streamingClient.SubscribeOrderbook(instrument.FIGI, *depth, requestID())
+func (s *mainScope) subscribeOrderbooks(streamingClient *sdk.StreamingClient) {
+	for _, ticker := range s.orderbookTickers {
+		instrument := s.tickerInstrument[ticker]
+		err := streamingClient.SubscribeOrderbook(instrument.FIGI, *orderbookDepth, requestID())
 		if err != nil {
-			logger.Fatalln(err)
+			s.logger.Fatalln(err)
 		}
-		logger.Println("Subscribed", instrument.Ticker, instrument.FIGI)
+		s.logger.Println("Subscribed to orderbook", instrument.Ticker, instrument.FIGI)
 	}
 }
 
-func (s *mainScope) unsubscribeOrderbooks(streamingClient *sdk.StreamingClient, logger *log.Logger) {
-	for _, instrument := range s.tickerInstrument {
-		err := streamingClient.UnsubscribeOrderbook(instrument.FIGI, *depth, requestID())
+func (s *mainScope) unsubscribeOrderbooks(streamingClient *sdk.StreamingClient) {
+	for _, ticker := range s.orderbookTickers {
+		instrument := s.tickerInstrument[ticker]
+		err := streamingClient.UnsubscribeOrderbook(instrument.FIGI, *orderbookDepth, requestID())
 		if err != nil {
-			logger.Fatalln(err)
+			s.logger.Fatalln(err)
 		}
-		logger.Println("Unsubscribed", instrument.Ticker, instrument.FIGI)
+		s.logger.Println("Unsubscribed from orderbook", instrument.Ticker, instrument.FIGI)
 	}
 }
 
-func (s *mainScope) initDiskWriters(logger *log.Logger) {
-	for _, t := range s.tickers {
-		ticker := t
-		go func() {
-			instrument := s.tickerInstrument[ticker]
-			ch := s.channels[figi(instrument.FIGI)]
-
-			filename, err := filepath.Abs(filepath.Join(".", string(ticker)))
-			if err != nil {
-				logger.Fatalln(err)
-			}
-
-			f, err := os.OpenFile(filename, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
-			if err != nil {
-				logger.Fatalln(err)
-			}
-
-			defer func() {
-				if err := f.Close(); err != nil {
-					logger.Fatalln(err)
-				}
-			}()
-
-			for event := range ch {
-				row := map[string]interface{}{
-					"time":   event.time.Format(time.RFC3339Nano),
-					"ticker": event.ticker,
-					"event":  event.event,
-				}
-
-				jsonBytes, err := json.Marshal(row)
-				if err != nil {
-					logger.Fatalln(err)
-				}
-
-				_, err = f.WriteString(fmt.Sprintf("%v\n", string(jsonBytes)))
-				if err != nil {
-					logger.Fatalln(err)
-				}
-			}
-		}()
+func (s *mainScope) subscribeCandles(streamingClient *sdk.StreamingClient) {
+	for _, ticker := range s.candleTickers {
+		instrument := s.tickerInstrument[ticker]
+		err := streamingClient.SubscribeCandle(instrument.FIGI, sdk.CandleInterval(*candleInterval), requestID())
+		if err != nil {
+			s.logger.Fatalln(err)
+		}
+		s.logger.Println("Subscribed to candles", instrument.Ticker, instrument.FIGI)
 	}
 }
 
-func flagTickers(flag string) (tickers []ticker) {
+func (s *mainScope) unsubscribeCandles(streamingClient *sdk.StreamingClient) {
+	for _, ticker := range s.candleTickers {
+		instrument := s.tickerInstrument[ticker]
+		err := streamingClient.UnsubscribeCandle(instrument.FIGI, sdk.CandleInterval(*candleInterval), requestID())
+		if err != nil {
+			s.logger.Fatalln(err)
+		}
+		s.logger.Println("Unsubscribed from candles", instrument.Ticker, instrument.FIGI)
+	}
+}
+
+func (s *mainScope) orderbookWriter(ch eventChannel, filePath string) {
+	file, err := os.OpenFile(filePath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		s.logger.Fatalln(err)
+	}
+
+	defer func() {
+		if err := file.Close(); err != nil {
+			s.logger.Fatalln(err)
+		}
+	}()
+
+	for wrappedEvent := range ch {
+		event := wrappedEvent.event.(sdk.OrderBookEvent)
+		row := map[string]interface{}{
+			"ticker":     wrappedEvent.ticker,
+			"figi":       figi(event.OrderBook.FIGI),
+			"time":       event.Time,
+			"local_time": wrappedEvent.time.Format(time.RFC3339Nano),
+			"bids":       event.OrderBook.Bids,
+			"asks":       event.OrderBook.Asks,
+		}
+
+		jsonBytes, err := json.Marshal(row)
+		if err != nil {
+			s.logger.Fatalln(err)
+		}
+
+		_, err = file.WriteString(fmt.Sprintf("%v\n", string(jsonBytes)))
+		if err != nil {
+			s.logger.Fatalln(err)
+		}
+	}
+}
+
+func (s *mainScope) candleWriter(ch eventChannel, filePath string) {
+	file, err := os.OpenFile(filePath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		s.logger.Fatalln(err)
+	}
+
+	defer func() {
+		if err := file.Close(); err != nil {
+			s.logger.Fatalln(err)
+		}
+	}()
+
+	for wrappedEvent := range ch {
+		event := wrappedEvent.event.(sdk.CandleEvent)
+		row := map[string]interface{}{
+			"ticker":     wrappedEvent.ticker,
+			"figi":       figi(event.Candle.FIGI),
+			"time":       event.Time,
+			"local_time": wrappedEvent.time.Format(time.RFC3339Nano),
+
+			"o":     event.Candle.OpenPrice,
+			"c":     event.Candle.ClosePrice,
+			"h":     event.Candle.HighPrice,
+			"l":     event.Candle.LowPrice,
+			"v":     event.Candle.Volume,
+			"time2": event.Candle.TS,
+			"i":     event.Candle.Interval,
+		}
+
+		jsonBytes, err := json.Marshal(row)
+		if err != nil {
+			s.logger.Fatalln(err)
+		}
+
+		_, err = file.WriteString(fmt.Sprintf("%v\n", string(jsonBytes)))
+		if err != nil {
+			s.logger.Fatalln(err)
+		}
+	}
+}
+
+func (s *mainScope) initDiskWriters() {
+	for _, ticker := range s.allTickers() {
+
+		instrument := s.tickerInstrument[ticker]
+		figi := figi(instrument.FIGI)
+
+		if _, ok := findTicker(s.orderbookTickers, ticker); ok {
+			filePath, err := filepath.Abs(filepath.Join(*path, string(ticker), "_orderbook"))
+			if err != nil {
+				s.logger.Fatalln(err)
+			}
+			ch := s.orderbookFigiChannels[figi]
+			go s.orderbookWriter(ch, filePath)
+		}
+
+		if _, ok := findTicker(s.candleTickers, ticker); ok {
+			filePath, err := filepath.Abs(filepath.Join(*path, string(ticker), "_candles"))
+			if err != nil {
+				s.logger.Fatalln(err)
+			}
+			ch := s.candlesFigiChannels[figi]
+			go s.candleWriter(ch, filePath)
+		}
+	}
+}
+
+func (s *mainScope) allTickers() []ticker {
+	tickers := append(s.orderbookTickers, s.candleTickers...)
+
+	table := map[ticker]bool{}
+	for _, ticker := range tickers {
+		table[ticker] = true
+	}
+
+	keys := make([]ticker, len(table))
+	i := 0
+	for k := range table {
+		keys[i] = k
+		i++
+	}
+
+	return keys
+}
+
+func listToTickers(flag string) (tickers []ticker) {
 	flagArr := strings.Split(flag, ",")
 	for _, str := range flagArr {
 		tickers = append(tickers, ticker(str))
 	}
 	return tickers
+}
+
+var letterRunes = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+
+func requestID() string {
+	b := make([]rune, 12)
+	for i := range b {
+		b[i] = letterRunes[rand.Intn(len(letterRunes))]
+	}
+
+	return string(b)
+}
+
+func findTicker(slice []ticker, val ticker) (int, bool) {
+	for i, item := range slice {
+		if item == val {
+			return i, true
+		}
+	}
+	return -1, false
 }
